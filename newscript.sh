1b4c9b8bd3ce13f64a2ceaf8743c508fcfd03816
mbv24 <mbv24@cornell.edu>
2017-07-13 12:56:30 -0400

    added newscript.sh

---
+++ newscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "You are in this directory: `pwd`"
+echo ""
+echo "These are the files in this directory: `ls -lah`"
+echo ""
+echo "Right now it is: `date --iso-8601`"
+echo ""
+echo "You are `whoami` on `hostname` (`hostname -i`)"
+
+
+
